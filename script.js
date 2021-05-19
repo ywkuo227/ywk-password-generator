1f433921699d7c6474486899ab5b6bc3e59bc52e
ywkuo227 <ywkuo@outlook.com>
2021-05-18 17:25:11 -0700

    May 28 code update #2
    Script:
    - Rewrite of the assign and error check logic. (4th iteration)
    - Completion of the password generator.
    - Return the generated password to the function to display in HTML.
    
    Known Issue:
    - Does not check for no character type selected. (Code is working, but minor bug. Need to investigate.)

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Create arrays for upper case letter, lower case letter, numerics and spcial characters.
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!","@","#","$","%","^","&","*","(",")",".","?","+","-","_","=",";",":","<",">","~","{","}"];
+var aryCharPool = [
+  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+  ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  ["!","@","#","$","%","^","&","*","(",")",".","?","+","-","_","=",";",":","<",">","~","{","}"]
+];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -21,175 +23,107 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   // Set array of userInput.
-  var userInput = [];
+  var aryUserInput = [];
+  var aryGenPool = [];
 
   // Set system messages.
+  var prmptMsg = [
+    "Does your password require UPPER case character? (Y/N)",
+    "Does your password require lower case character? (Y/N)",
+    "Does your password require numeric character? (Y/N)",
+    "Does your password require special character? (Y/N)",
+    "Please enter your desired password length (8-128 characters):"
+  ];
   var cfmQuitGen = "Do you wish to quit generator?"
-  var alrtInputNo = "Please note input is set to 'No'."
+  var alrtInputNo = "Please note the input is set to 'No'."
   var alrtCancel = "Generator will now stop.";
   var alrtNeedInput = "Please enter 'Y'es or 'N'o. (Y/N)";
-  var alrtNotVldNum = "Please enter a valid number between 1-128.";
+  var alrtNotVldNum = "Please enter a valid number between 8-128.";
 
   // User input variables
-  var incldUpper;
-  var incldLower;
-  var incldNum;
-  var incldSpcl;
-  var pwLength;
+  var prmptInput
+  var i = 0;
+  var ii;
   var quitGen;
-  var cancel = "cancel"
-  
-  // Function to assign upper case input and error check.
-  function usrInputUpper(incldUpper = prompt("Does your password require UPPER case character? (Y/N)")) {
-  
-    if (incldUpper === null) {
-      quitGen = confirm(cfmQuitGen);
-      if (quitGen === true) {
-        userInput[0] = cancel;
+
+  // Output variable. Reset upon execution.
+  var outputPw = "";
+
+  // Function to assign user input to userInput array, as well as all logic and error check.
+  function assignUserInput() {
+    if (i >= 0 && i <= 4) {
+      prmptInput = prompt(prmptMsg[i]);
+      if (prmptInput === null) {
+        quitGen = confirm(cfmQuitGen);
+        if (quitGen === true) {
+          aryUserInput[i] = "cancel";
+        } else {
+          if (ii === 1) {
+            if (i <= 3) {
+              alert(alrtInputNo);
+              aryUserInput[i] = false;
+            } else if (i === 4) {
+              alert("Please note the input is set to minimum password length '8' characters.");
+              aryUserInput[i] = "8";
+            }
+            ii = 0;
+          } else {
+            ii = 0;
+            ii ++;
+            assignUserInput(i);
+          }
+        }
+      } else if ((prmptInput === "Y") || (prmptInput === "N") || (prmptInput === "y") || (prmptInput === "n")) {
+        if ((prmptInput === "Y") || (prmptInput === "y")) {
+          aryUserInput[i] = true;
+        } else if ((prmptInput === "N") || (prmptInput === "n")){
+          aryUserInput[i] = false;
+        }
+      } else if ((prmptInput >= 8 && prmptInput <= 128) === true) {
+        aryUserInput[i] = prmptInput;
       } else {
-        alert(alrtInputNo);
-        userInput[0] = false;
+        if (i <= 3) {
+          alert(alrtNeedInput);
+        } else if (i === 4) {
+          alert(alrtNotVldNum);
+        }
+        assignUserInput(i);
       }
-    } else if ((incldUpper === "Y") || (incldUpper === "N") || (incldUpper === "y") || (incldUpper === "n")) {
-      if ((incldUpper === "Y") || (incldUpper === "y")) {
-        userInput[0] = true;
-      } else if ((incldUpper === "N") || (incldUpper === "n")){
-        userInput[0] = false;
-      }
-    } else {
-      alert(alrtNeedInput);
-      usrInputUpper();
     }
   }
 
-  // Function to assign lower case input and error check.
-  function usrInputLower(incldLower = prompt("Does your password require lower case character? (Y/N)")) {
-    
-    if (incldLower === null) {
-      quitGen = confirm(cfmQuitGen);
-      if (quitGen === true) {
-        userInput[1] = cancel;
-      } else {
-        alert(alrtInputNo);
-        userInput[1] = false;
+  // Ask user for password criteria and terminate the excution if user chose to quit.
+  for (i = 0; i <= 4; i++) {
+    assignUserInput(i);
+    if (aryUserInput[i] === "cancel") {
+      alert(alrtCancel);
+      return outputPw = "";
+    } 
+    // else if ((aryUserInput[0] === false) && (aryUserInput[1] === false) && (aryUserInput[2] === false) && (aryUserInput[3] === false)) {
+    //   alert("Please select at least one character type.");
+    //   i=0;
+    //   assignUserInput(i);
+    // } 
+    else if ((i <= 3) && (aryUserInput[i] === true)) {
+      for (var j = 0; j < aryCharPool[i].length; j++) {
+        aryGenPool.push(aryCharPool[i][j]);
       }
-    } else if ((incldLower === "Y") || (incldLower === "N") || (incldLower === "y") || (incldLower === "n")) {
-      if ((incldLower === "Y") || (incldLower === "y")) {
-        userInput[1] = true;
-      } else if ((incldLower === "N") || (incldLower === "n")){
-        userInput[1] = false;
-      }
-    } else {
-      alert(alrtNeedInput);
-      usrInputLower();
     }
-
   }
 
-  // Function to assign number input and error check.
-  function usrInputNum(incldNum = prompt("Does your password require numeric character? (Y/N)")) {
-    
-    if (incldNum === null) {
-      quitGen = confirm(cfmQuitGen);
-      if (quitGen === true) {
-        userInput[2] = cancel;
-      } else {
-        alert(alrtInputNo);
-        userInput[2] = false;
-      }
-    } else if ((incldNum === "Y") || (incldNum === "N") || (incldNum === "y") || (incldNum === "n")) {
-      if ((incldNum === "Y") || (incldNum === "y")) {
-        userInput[2] = true;
-      } else if ((incldNum=== "N") || (incldNum === "n")){
-        userInput[2] = false;
-      }
-    } else {
-      alert(alrtNeedInput);
-      usrInputNum();
-    }
-
+  // Generate password.
+  for (var numChar = 1; numChar <= aryUserInput[4]; numChar++) {
+    var idxPwGen = Math.floor(Math.random()*aryGenPool.length);
+    outputPw = outputPw + aryGenPool[idxPwGen];
   }
 
-  // Function to assign special character input and error check.
-  function usrInputSpcl(incldSpcl = prompt("Does your password require special character? (Y/N)")) {
-    
-    if (incldSpcl === null) {
-      quitGen = confirm(cfmQuitGen);
-      if (quitGen === true) {
-        userInput[3] = cancel;
-      } else {
-        alert(alrtInputNo);
-        userInput[3] = false;
-      }
-    } else if ((incldSpcl === "Y") || (incldSpcl === "N") || (incldSpcl === "y") || (incldSpcl === "n")) {
-      if ((incldSpcl === "Y") || (incldSpcl === "y")) {
-        userInput[3] = true;
-      } else if ((incldSpcl=== "N") || (incldSpcl === "n")){
-        userInput[3] = false;
-      }
-    } else {
-      alert(alrtNeedInput);
-      usrInputSpcl();
-    }
+  // Bug checks. REMOVE BEFORE FLIGHT
+  console.log(aryUserInput);
+  console.log(aryGenPool);
+  console.log(aryGenPool.length);
+  console.log(outputPw);
+  console.log(outputPw.length);
 
-  }
-
-  // Function to assign password length and error check.
-  function usrInputPwLgth(pwLength = prompt("Please enter your desired password length (8-128 characters):")) {
-
-    if (pwLength === null) {
-      quitGen = confirm(cfmQuitGen);
-      if (quitGen === true) {
-        userInput[4] = cancel;
-      } else {
-        alert(alrtInputNo);
-        userInput[4] = false;
-      }
-    } else if ((pwLength >= 8 && pwLength <= 128) === true) {
-      userInput[4] = pwLength;
-    } else {
-      alert(alrtNotVldNum);
-      usrInputPwLgth();
-    }
-
-  }
-
-  usrInputUpper();
-  
-  if (userInput[0] === cancel) {
-    alert(alrtCancel);
-    return;
-  } else {
-    usrInputLower();
-  }
-
-  if (userInput[1] === cancel) {
-    alert(alrtCancel);
-    return;
-  } else {
-    usrInputNum();
-  }
-
-  if (userInput[2] === cancel) {
-    alert(alrtCancel);
-    return;
-  } else {
-    usrInputSpcl();
-  }
-
-  if (userInput[3] === cancel) {
-    alert(alrtCancel);
-    return;
-  } else {
-    usrInputPwLgth();
-  }
-
-  if (userInput[4] === cancel) {
-    alert(alrtCancel);
-    return;
-  }
-
-  // Checkpoint. REMOVE BEFORE FLIGHT
-  console.log(userInput);
-
+  // Output generated password.
+  return outputPw;
 }
